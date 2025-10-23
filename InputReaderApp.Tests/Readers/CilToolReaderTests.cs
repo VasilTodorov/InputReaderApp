9c60b072dab9dbf9e0ecbead03854de7113061ae
Васил Тодоров <vasilit@office365student.uni-sofia.bg>
2025-10-23 11:47:10 +0300

    Implamented CilToolReader

---
+++ InputReaderApp.Tests/Readers/CilToolReaderTests.cs
@@ -0,0 +1,63 @@
+﻿using InputReaderApp.Readers;
+using InputReaderApp.Tests.Helpers;
+using InputReaderApp.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputReaderApp.Tests.Readers
+{
+    public class CilToolReaderTests
+    {
+        [Fact]
+        public void Read_ShouldReturnCommand()
+        {
+            //Arrange
+            string input = "command -f test.csv -o /output --quiet";
+            Command expected = new Command("test.csv", "/output", true);            
+
+            //Act
+            CilToolReader reader = new CilToolReader(new StringReader(input));
+            Result<Command> result = reader.Read();
+
+            //Assert
+            Assert.True(result.IsSuccess);
+            Assert.Equal(expected, result.Data!);
+        }
+
+        [Theory]
+        [InlineData("command -f test.csv -o /output")]
+        [InlineData("command -o /output -f test.csv")]
+        [InlineData("command --quiet -o /output -f test.csv")]
+        [InlineData("command -f test.csv --quiet -o /output")]
+        public void Read_ShouldSucceed_OnVariousValidInputs(string input)
+        {
+            //Act
+            var reader = new CilToolReader(new StringReader(input));
+            Result<Command> result = reader.Read();
+
+            //Assert
+            Assert.True(result.IsSuccess, result.Message);
+        }
+
+        [Theory]
+        [InlineData("command -f test.csv -f hello.txt -o /output --quiet")]
+        [InlineData("command -f -f test.csv -o /output --quiet")]
+        [InlineData("command -f test.csv -o /output -quiet")]
+        [InlineData("command -f test.csv --quiet")]
+        [InlineData("command -f test.csv --quiet -o")]
+        [InlineData("-f test.csv -o /output")]
+        public void Read_ShouldFail_OnVariousInvalidInputs(string input)
+        {
+            //Act
+            var reader = new CilToolReader(new StringReader(input));
+            Result<Command> result = reader.Read();
+
+            //Assert
+            Assert.True(result.IsFailure, result.Message);
+            Assert.Equal(ErrorCode.InvalidFormat, result.Code);
+        }
+    }
+}
--- InputReaderApp/Readers/CilToolReader.cs
+++ InputReaderApp/Readers/CilToolReader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InputReaderApp.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,58 @@ using System.Threading.Tasks;
 
 namespace InputReaderApp.Readers
 {
-    public class CilToolReader
+    public record Command(string Input, string Output, bool Quiet);
+    public class CilToolReader : ReaderBase<Command>
     {
+        public CilToolReader(TextReader? input = null) : base(input)
+        {
+        }
+
+        public override Result<Command> Read()
+        {
+            string? line = Input.ReadLine();          
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                return Result<Command>.Fail(ErrorCode.InputNotFound, "Error(1) : input not found");
+            }
+
+            var tokens = line.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            string? input = null ;
+            string? output = null;
+            bool quiet = false;            
+
+            if (tokens[0] != "command")
+                return Result<Command>.Fail(ErrorCode.InvalidFormat, "Error(2) : command not found");
+
+            for ( int i = 1; i < tokens.Length; i++)
+            {
+                switch (tokens[i])
+                {
+                    case "-f":
+                        if (input is not null)
+                            return Result<Command>.Fail(ErrorCode.InvalidFormat, "Error(3) : Duplicate input option (-f) found");
+                        if (i+1<tokens.Length) 
+                            input = tokens[++i]; 
+                        break;
+                    case "-o":
+                        if (output is not null)
+                            return Result<Command>.Fail(ErrorCode.InvalidFormat, "Error(4) :Duplicate output option (-o) found");
+                        if (i + 1 < tokens.Length) 
+                            output = tokens[++i];
+                        break;
+                    case "--quiet":
+                        quiet = true;
+                        break;
+                    default:
+                        return Result<Command>.Fail(ErrorCode.InvalidFormat, $"Error(5) : couldn't recognize: {tokens[i]}.");
+                }
+            }
+            if(input is null)
+                return Result<Command>.Fail(ErrorCode.InvalidFormat, "Error(6) : input not found");
+            if (output is null)
+                return Result<Command>.Fail(ErrorCode.InvalidFormat, "Error(7) : output not found");
+
+            return Result<Command>.Success(new Command(input, output, quiet));
+        }
     }
 }
